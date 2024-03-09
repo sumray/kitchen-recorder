49bd9bc32b4ac586dbae2013b15dfd2b6c003ccc
sumray <543576868@qq.com>
2024-03-09 12:44:50 +0800

    删除无用导包

--- src/main/java/zq/project/account/AccountController.java
+++ src/main/java/zq/project/account/AccountController.java
@@ -1,7 +1,5 @@
 package zq.project.account;
 
-import org.apache.ibatis.annotations.InsertProvider;
-import org.apache.ibatis.plugin.Interceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import zq.project.common.ResultMessage;
